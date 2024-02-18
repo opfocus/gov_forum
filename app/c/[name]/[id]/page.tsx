d5c960f1105982878793a142cad0b81c472dc1e4
simple8720 <86885634+opfocus@users.noreply.github.com>
2024-02-18 20:30:24 +0800

    Update page.tsx
    Removed ResolvedMetadata type and related code that caused errors during deployment (currently no impact, will attempt to modify in future work)

--- app/c/[name]/[id]/page.tsx
+++ app/c/[name]/[id]/page.tsx
@@ -2,22 +2,20 @@ import Processing from "@/ui/processing";
 import TopicsList from "@/ui/topics-list";
 import { Suspense } from "react";
 
-import type { Metadata, ResolvedMetadata } from "next";
+import type { Metadata} from "next";
 import { getCategories } from "@/utils/getCategories";
 
 export async function generateMetadata({params}: {
   params: {id: string}
-}, parent: ResolvedMetadata): Promise<Metadata> {
+}): Promise<Metadata> {
   const id = params.id
   const categories = await getCategories()
 
   const [item] = categories.filter((category) => category.id === Number(id))
-  const previousTitle = (await parent).title
-  const previousDescription = (await parent).description
 
   return {
-    title: item?.name || previousTitle,
-    description: item?.description || previousDescription,
+    title: item?.name,
+    description: item?.description,
   }
 }
 
