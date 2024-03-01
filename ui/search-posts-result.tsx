819a0e58e8468ed2a3da1193450edf4a991861e9
opfocus <wangzhsr@gmail.com>
2024-03-01 22:26:24 +0800

    Initial Implementation of Full-Text Search UI and Functionality

---
+++ ui/search-posts-result.tsx
@@ -0,0 +1,28 @@
+'use client'
+import type { Post } from "@/lib/type"
+import Image from "next/image"
+
+export default function SearchPostsResult({posts}: {
+  posts:Post[]
+}) {
+  return (
+    <div className=" w-full space-y-4">
+    {
+      posts.map(post =>(
+        <div key={post.id} className="flex flex-row justify-start border-b pb-4 border-gray-400 border-solid">
+          <div>
+            <img
+              src={post.avatar_template}
+              className=" w-8 h-8 rounded-full"
+              alt="use avatar"
+            >
+            </img>
+          </div>
+          <div  dangerouslySetInnerHTML={{__html: post.cooked}} className=" grow pl-4">
+          </div>
+        </div>
+      ))
+    }
+    </div>
+  )
+}
